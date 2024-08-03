ffce3933e8b7bc3523e8e290fd971c01c1b062bd
Sadanandlrnr <68555977+Sadanandlrnr@users.noreply.github.com>
2024-08-03 16:44:15 +0530

    Update Scratch.java

--- src/main/java/org/example/Scratch.java
+++ src/main/java/org/example/Scratch.java
@@ -6,8 +6,8 @@ public class Scratch {
 
     public static void main( String[] args ) {
 
-        String api_key = "7276e037fed5a44a1968672dae7577fd";
-        String app_key = "79c2e03d7220f018c2b880657f476a11121295fe";
+        String api_key = "7276e037fed5a44a1968672dae7577fdl";
+        String app_key = "79c2e03d7220f018c2b880657f476a11121295fel";
 
         HashMap<String,String>  secret = new HashMap<>();
 
